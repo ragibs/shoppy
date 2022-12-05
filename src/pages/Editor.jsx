c98bc20a360bd41186a10b0556395e5c92e766de
Ragib Sina <ragib.sina@gmail.com>
2022-12-05 14:00:33 -0700

    updated favicon and name

--- src/pages/Editor.jsx
+++ src/pages/Editor.jsx
@@ -1,8 +1,16 @@
-import React from 'react';
-import { HtmlEditor, Image, Inject, Link, QuickToolbar, RichTextEditorComponent, Toolbar } from '@syncfusion/ej2-react-richtexteditor';
+import React from "react";
+import {
+  HtmlEditor,
+  Image,
+  Inject,
+  Link,
+  QuickToolbar,
+  RichTextEditorComponent,
+  Toolbar,
+} from "@syncfusion/ej2-react-richtexteditor";
 
-import { Header } from '../components';
-import { EditorData } from '../data/dummy';
+import { Header } from "../components";
+import { EditorData } from "../data/dummy";
 
 const Editor = () => (
   <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
